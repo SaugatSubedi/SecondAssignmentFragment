65ee52c9956cd09eae0614d33200b5a4719cfe3b
Saugat Subedi <subedisaugat176@gmail.com>
2019-12-10 21:10:38 +0545

    armstrong added

--- app/src/main/java/com/saugat/secondassignmentfragment/fragment/Armstrong.java
+++ app/src/main/java/com/saugat/secondassignmentfragment/fragment/Armstrong.java
@@ -8,14 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.saugat.secondassignmentfragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Armstrong extends Fragment {
+public class Armstrong extends Fragment implements View.OnClickListener{
 
+    private Button btnCalculateArmstrong;
+    private EditText etArmstrongNumber;
+    private TextView tvArmstrongResult;
 
     public Armstrong() {
         // Required empty public constructor
@@ -26,7 +32,34 @@ public class Armstrong extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_armstrong, container, false);
+        View view = inflater.inflate(R.layout.fragment_armstrong, container, false);
+
+        etArmstrongNumber = view.findViewById(R.id.etArmstrongNumber);
+        btnCalculateArmstrong = view.findViewById(R.id.btnCalculateArmstrong);
+        tvArmstrongResult = view.findViewById(R.id.tvArmstrongResult);
+
+        btnCalculateArmstrong.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int c = 0, a, temp;
+
+        int n = Integer.parseInt(etArmstrongNumber.getText().toString());
+
+        temp = n;
+
+        while(n > 0){
+            a = n%10;
+            n = n/10;
+            c = c+(a*a*a);
+        }
+
+        if (temp == c){
+            tvArmstrongResult.append(temp + " is a armstrong number.\n");
+        }
+        else {
+            tvArmstrongResult.append(temp + " is not a armstrong number.\n");
+    }
 }
