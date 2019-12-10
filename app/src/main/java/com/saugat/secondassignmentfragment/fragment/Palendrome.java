30d488eca089031bc835adf84e7273df1eb2ab8b
Saugat Subedi <subedisaugat176@gmail.com>
2019-12-10 20:12:18 +0545

    Palindrome added

--- app/src/main/java/com/saugat/secondassignmentfragment/fragment/Palendrome.java
+++ app/src/main/java/com/saugat/secondassignmentfragment/fragment/Palendrome.java
@@ -8,14 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.saugat.secondassignmentfragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Palendrome extends Fragment {
+public class Palendrome extends Fragment implements View.OnClickListener {
 
+    private Button btnCalculatePalindrome;
+    private EditText etPalindrome;
+    private TextView tvPalindromeResult;
 
     public Palendrome() {
         // Required empty public constructor
@@ -26,7 +32,38 @@ public class Palendrome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_palendrome, container, false);
+        View view = inflater.inflate(R.layout.fragment_palendrome, container, false);
+
+        etPalindrome = view.findViewById(R.id.etPalindrome);
+        btnCalculatePalindrome = view.findViewById(R.id.btnCalculatePalindrome);
+        tvPalindromeResult = view.findViewById(R.id.tvPalindromeResult);
+
+        btnCalculatePalindrome.setOnClickListener(this);
+        return view;
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+        int r, sum = 0, temp;
+
+        int n = Integer.parseInt(etPalindrome.getText().toString());
+
+        temp = n;
+
+        while (n > 0) {
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+
+        if (temp == sum) {
+            tvPalindromeResult.append(temp + " is a palindrome number.\n");
+        } else {
+            tvPalindromeResult.append(temp + " is not a palindrome number.\n");
+
+        }
+    }
 }
