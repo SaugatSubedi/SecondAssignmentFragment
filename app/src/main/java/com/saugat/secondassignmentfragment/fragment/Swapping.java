0318701710a5e63e03c116de9baf816eea0bfc08
Saugat Subedi <subedisaugat176@gmail.com>
2019-12-10 21:33:56 +0545

    swapping added

--- app/src/main/java/com/saugat/secondassignmentfragment/fragment/Swapping.java
+++ app/src/main/java/com/saugat/secondassignmentfragment/fragment/Swapping.java
@@ -8,14 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.saugat.secondassignmentfragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Swapping extends Fragment {
-
+public class Swapping extends Fragment implements View.OnClickListener{
+    private Button btnCalculateSwap;
+    private EditText etFirstNumber, etSecondNumber;
+    private TextView tvSwapResult;
 
     public Swapping() {
         // Required empty public constructor
@@ -26,7 +31,34 @@ public class Swapping extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swapping, container, false);
+        View view = inflater.inflate(R.layout.fragment_swapping, container, false);
+
+        etFirstNumber = view.findViewById(R.id.etFirstNumber);
+        etSecondNumber = view.findViewById(R.id.etSecondNumber);
+        btnCalculateSwap = view.findViewById(R.id.btnCalculateSwap);
+        tvSwapResult = view.findViewById(R.id.tvSwapResult);
+
+        btnCalculateSwap.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        int first,second,temp, bFirst, bSecond;
+
+        first = Integer.parseInt(etFirstNumber.getText().toString());
+        second = Integer.parseInt(etSecondNumber.getText().toString());
+
+        bFirst = first;
+        bSecond = second;
+
+
+        temp = first;
+        first = second;
+        second = temp;
+
+        tvSwapResult.append("The value before swapping. \n First Number is "+ bFirst + "\n Second Number "
+                + bSecond +".\n The value after swapping.\n First number is " + first + " \n Second Number is "+ second + ".\n");
+    }
 }
